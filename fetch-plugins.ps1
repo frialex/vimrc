c8f3ef43f0e851509902205e068fabd816dae140
Frison Alexander <soggyfri@gmail.com>
2015-06-17 20:37:38 -0400

    Start work on restoration script

--- fetch-plugins.ps1
+++ fetch-plugins.ps1
@@ -1,31 +1,13 @@
-#Put me in the bundle dir and execute!
+$oldErrorPreference = $ErrorActionPreference
+$ErrorActionPreference = 'stop'
 
-$bundleDir = Split-Path $MyInvocation.MyCommand.Path
-Push-Location $bundleDir
-
-$plugins = @{
-    "ctrlp.vim-master" = "https://github.com/kien/ctrlp.vim.git";
-    "emmet-vim" = "https://github.com/mattn/emmet-vim.git";
-    "MatchTagAlways-master" = "https://github.com/Valloric/MatchTagAlways.git";
-    "nerdtree" = "https://github.com/scrooloose/nerdtree.git";
-    "tabular" = "https://github.com/godlygeek/tabular.git";
-    "vim-airline" = "https://github.com/bling/vim-airline.git";
-    "vim-easymotion-master" = "https://github.com/Lokaltog/vim-easymotion.git";
-    "vim-fugitive" = "https://github.com/tpope/vim-fugitive.git";
-    "vim-jade-master" = "https://github.com/digitaltoad/vim-jade.git";
-    "vim-surround" = "https://github.com/tpope/vim-surround.git";
-    "vim-unimpaired" = "https://github.com/tpope/vim-unimpaired.git";
-    "zeavim.vim" = "https://github.com/KabbAmine/zeavim.vim.git";
-    "gundo.vim" = "https://github.com/sjl/gundo.vim.git";
-    "nerdcommenter" = "https://github.com/scrooloose/nerdcommenter.git"
+try { if(Get-Command choco) {
+#install vim, git, clone vimrc, autohotkey, clone keycommands, chrome, cmdr, pscx, node, 
+}}
+catch {
+    Write-Host "INSTALLING CHOCOLATY"
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-
-
-$plugins.keys | % {
-    if(Test-Path $_){
-        Write-Host "$_ is installed"
-    } else {
-        Write-Host "Installing $($plugins.Item($_))"
-        git clone $plugins.Item($_)
-    }
+finally {
+    $ErrorActionPreference = $oldErrorPreference
 }
